4f07f58cff806cf4eda24e6843ce02aca51dfe49
daniel3g <silvadaniel2006@yahoo.com.br>
2023-11-07 19:11:41 -0300

    finish front-end aplication

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,6 @@
 const menuToggle = document.querySelector('.menu-toggle');
 const nav = document.querySelector('#menu');
+const item = document.querySelector('.nav-menu');
 
 menuToggle.addEventListener("click", () => nav.classList.toggle("active"));
+item.addEventListener("click", () => nav.classList.toggle("active"));
